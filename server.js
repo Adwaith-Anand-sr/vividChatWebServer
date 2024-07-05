f359b572740cc2f031fd1989036858e2f7131143
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-07-05 21:46:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
    cors: {
-      origin: ['http://localhost:5173', 'https://vividchatwebserver.onrender.com']
+      origin: ['http://localhost:5173', 'https://vividchat.onrender.com']
    }
 })
 
